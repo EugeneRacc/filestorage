7612ed6c98fc5c84e3b404273df8dfd405bfdb36
EugeneRacc <forjbracc@gmail.com>
2022-06-11 13:13:38 +0300

    Create Business project and WebApi and add connection string to appsettings

---
+++ Server/WebApi/WebApi/Startup.cs
@@ -0,0 +1,43 @@
+using Data.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi;
+
+public class Startup
+{
+   
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<FileStorageDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("FileStorage")));
+        
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        
+        }
+    }
+
