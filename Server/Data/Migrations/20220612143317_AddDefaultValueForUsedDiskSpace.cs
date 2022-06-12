e86eaeae87e3dd1cd245341de07a48fb258f5f61
EugeneRacc <my email address used on my git repo>
2022-06-12 17:38:11 +0300

    Update DB with adding default value to UsedDiskSpace

---
+++ Server/Data/Migrations/20220612143317_AddDefaultValueForUsedDiskSpace.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class AddDefaultValueForUsedDiskSpace : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UsedDiskSpade",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "0",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UsedDiskSpade",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValue: "0");
+        }
+    }
+}
