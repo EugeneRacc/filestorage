14623814bd029e12fdaa7ea5937d3e9ee10983f6
Evgenii Ivanchenko <forjbracc@gmail.com>
2022-06-26 21:08:47 +0300

    Add migration with better foreign keys in db

---
+++ Server/Data/Migrations/20220626175536_ChangeFileForeignKey.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class ChangeFileForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Files_Files_ChildId",
+                table: "Files");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Files_ChildId",
+                table: "Files");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_ParentId",
+                table: "Files",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Files_Files_ParentId",
+                table: "Files",
+                column: "ParentId",
+                principalTable: "Files",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Files_Files_ParentId",
+                table: "Files");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Files_ParentId",
+                table: "Files");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_ChildId",
+                table: "Files",
+                column: "ChildId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Files_Files_ChildId",
+                table: "Files",
+                column: "ChildId",
+                principalTable: "Files",
+                principalColumn: "Id");
+        }
+    }
+}
