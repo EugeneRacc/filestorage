8a1a0ecf1581b9e1bc32ab71fc1e7abd987a54ce
Evgenii Ivanchenko <forjbracc@gmail.com>
2022-07-04 10:10:12 +0300

    Remove System.Linq

--- Server/WebApi/WebApi/Controllers/AdminController.cs
+++ Server/WebApi/WebApi/Controllers/AdminController.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApi.Controllers
